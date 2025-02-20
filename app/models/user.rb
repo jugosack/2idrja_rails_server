0786a32015e37ef853848a20a8cc807694630fde
jugosack <jugoslav_ackoski@yahoo.com>
2025-02-20 20:02:53 +0100

    Add setup project

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :confirmable,
+         :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :country, presence: true
+  validates :mobile_number, presence: true
+  validates :terms_of_use, acceptance: true
+
+  attr_accessor :unconfirmed_email
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get '/current_user', to: 'current_user#index'
+  devise_for :users, path: '', path_names: {
+                                 sign_in: 'login',
+                                 sign_out: 'logout',
+                                 registration: 'signup',
+                                 confirmation: 'confirm'
+                               },
+                     controllers: {
+                       sessions: 'users/sessions',
+                       registrations: 'users/registrations',
+                       confirmations: 'users/confirmations'
+                     }
+end
