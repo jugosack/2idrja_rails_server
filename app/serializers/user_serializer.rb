9b280f17181021e71310896ec9b28dac029e29b1
jugosack <jugoslav_ackoski@yahoo.com>
2025-04-22 15:32:55 +0200

    Fixing rubocop errors

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :first_name, :last_name, :country, :mobile_number,:created_at
+  attributes :id, :email, :first_name, :last_name, :country, :mobile_number, :created_at
 end
