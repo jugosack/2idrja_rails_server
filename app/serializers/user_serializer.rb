11a656638c83241c0044d989b09e132b8d2786d7
jugosack <jugoslav_ackoski@yahoo.com>
2025-05-28 19:42:39 +0200

    Fixing avatar image

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,16 @@
+# class UserSerializer
+#   include JSONAPI::Serializer
+#   attributes :id, :email, :first_name, :last_name, :country, :mobile_number, :created_at
+# end
+
 class UserSerializer
   include JSONAPI::Serializer
+
   attributes :id, :email, :first_name, :last_name, :country, :mobile_number, :created_at
+
+  attribute :avatar_url do |user|
+    if user.avatar.attached?
+      Rails.application.routes.url_helpers.rails_blob_url(user.avatar, only_path: false)
+    end
+  end
 end
