7e29cb65810a6c7c469682d2fc432a700b65e7b3
jugosack <jugoslav_ackoski@yahoo.com>
2025-05-28 19:46:30 +0200

    Fixing rubocop errors

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -7,10 +7,11 @@ class UserSerializer
   include JSONAPI::Serializer
 
   attributes :id, :email, :first_name, :last_name, :country, :mobile_number, :created_at
-
+  # rubocop:disable Style/IfUnlessModifier
   attribute :avatar_url do |user|
     if user.avatar.attached?
       Rails.application.routes.url_helpers.rails_blob_url(user.avatar, only_path: false)
     end
   end
+  # rubocop:enable Style/IfUnlessModifier
 end
