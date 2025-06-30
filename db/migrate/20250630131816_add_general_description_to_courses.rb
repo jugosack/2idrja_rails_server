a7158139fca3b9d557fbb0d6c809f11dd122fe69
jugosack <jugoslav_ackoski@yahoo.com>
2025-06-30 15:31:51 +0200

    Fixing rubocop errors

--- db/migrate/20250630131816_add_general_description_to_courses.rb
+++ db/migrate/20250630131816_add_general_description_to_courses.rb
@@ -1,5 +1,5 @@
 class AddGeneralDescriptionToCourses < ActiveRecord::Migration[7.0]
   def change
-   add_column :courses, :general_description, :text, null: false, default: "To be added"
+    add_column :courses, :general_description, :text, null: false, default: 'To be added'
   end
 end
